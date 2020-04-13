fdd99571ea73d0d1484c4941fedb42d0e9fe7462
Rishabh Kanojiya <rishabhkanojiya75@gmail.com>
2020-04-13 22:59:37 +0530

    updating uri

--- client/src/index.js
+++ client/src/index.js
@@ -3,37 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
+import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import { onError } from "apollo-link-error";
-import { ApolloLink } from "apollo-link";
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: "http://localhost:5000/graphql",
-      credentials: "same-origin"
-    })
-  ]),
-  cache: new InMemoryCache(),
+  uri: `http://localhost:${process.env.PORT}/graphql`,
+  credentials: "include",
   dataIdFromObject: o => o.id
 });
-// const client = new ApolloClient({
-//   uri: "http://localhost:5000/graphql",
-//   // credentials: "include",
-//   dataIdFromObject: o => o.id
-// });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
