5faf17eeea3941d00b1bca6d60e14c46ad37f4a2
Rishabh Kanojiya <rishabhkanojiya75@gmail.com>
2020-04-13 21:18:06 +0530

    update heroku set

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   const path = require("path");
 
