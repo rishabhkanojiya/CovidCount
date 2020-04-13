646ab2164386f85a413c66f91c6e9f3e703ba626
Rishabh Kanojiya <rishabhkanojiya75@gmail.com>
2020-04-14 00:31:14 +0530

    read.md

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const expressGraphql = require("express-graphql");
-const schema = require("./schema/schema");
+const schema = require("./src/schema/schema");
 
 const app = express();
 
@@ -23,10 +23,9 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
 
   const path = require("path");
-
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
